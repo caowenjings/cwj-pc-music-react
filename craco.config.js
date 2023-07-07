410cf79b9b22c44472c5d139e429e9efe7800aea
caowenjings <932142752@qq.com>
2023-07-07 11:06:58 +0800

    feat: 添加配置别名

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+/** 不暴露的情况下修改webpack配置 */
+const path = require('path')
+
+const resolve = (dir) => path.resolve(__dirname, dir)
+
+module.exports = {
+  webpack: {
+    //自定义的别名
+    alias: {
+      '@': resolve('src'),
+      components: resolve('src/components')
+    }
+  }
+}
